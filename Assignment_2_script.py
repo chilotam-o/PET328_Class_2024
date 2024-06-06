8bb403d760c08419d72983b510911dd2531e14ec
chilotam-o <104756983+chilotam-o@users.noreply.github.com>
2024-06-06 16:46:48 +0100

    Update Assignment_2_script.py

--- Assignment_2_script.py
+++ Assignment_2_script.py
@@ -14,10 +14,6 @@ h = input("What is the thickness of the reservoir")
 sw = input("What is the water saturation of the reservoir")
 boi = input("What is formation volume factor of the reservoir fluid")
 
-# Calculating STOIIP
-N = (7758*area*h*poro*(1-sw))/boi
-
-
 # The input parameters received with the Function input
 # are encoded as strings, not as numbers.
 # They needed to be converted to numbers for them
@@ -29,6 +25,8 @@ h = float(h)
 sw = float(sw)
 boi = float(boi)
 
+# Calculating STOIIP
+N = (7758*area*h*poro*(1-sw))/boi
 
 # Displaying the output
 print('The amount of oil initially in place is ', N)
